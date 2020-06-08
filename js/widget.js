51613795639a5117dcc4344fea4d8e0050b6f751
Vaterpejekos <slusarev.lg@gmail.com>
2020-06-08 18:43:58 +0300

    first commit

---
+++ js/widget.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+let listEl = document.querySelector('[data-list]');
+let tmpl = document.querySelector('[data-tmpl]').innerHTML;
+
+
+window.addEventListener('load', sendRequest)
+
+function sendRequest(){
+	let url = '../data/employees.json';
+
+	fetch(url)
+		.then( result => result.json() )
+		.then( showData );	
+
+	
+}
+
+function showData(params) {
+	listEl.innerHTML = '';
+	params.forEach(element => {
+		if (element.inoffice == false) {
+			listEl.innerHTML += tmpl
+				.replace(/{{class}}/gi, "out")
+				.replace(/{{name}}/gi, element.name)
+				;
+		}else{
+			listEl.innerHTML += tmpl
+				.replace(/{{class}}/gi, "in")
+				.replace(/{{name}}/gi, element.name)
+				;
+		}
+	});//for
+}
